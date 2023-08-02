40a114639d3e7e2fac2767ebb71fd8a0f1d2ced8
Alperen Aydın <108737692+rybetasz@users.noreply.github.com>
2023-08-02 09:59:06 +0300

    Update setup.py

--- MtlExport/setup.py
+++ MtlExport/setup.py
@@ -1,13 +1,11 @@
 import sys
 from cx_Freeze import setup, Executable
-
-# Uygulamanın ana dosyasının adını ve yolu (MTLExport.py'yi kendi dosyanızın adıyla değiştirin)
+
 main_file = 'MTLExport.py'
 
-# cx_Freeze yapısında kullanılacak Executable nesnesini oluşturuyoruz
 executable = Executable(script=main_file, base='Win32GUI', icon='mtlexport.ico')
 
-# cx_Freeze setup fonksiyonunu çağırıyoruz
+
 setup(
     name='MTLExport',
     version='1.0',
